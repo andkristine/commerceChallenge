46f390df301a60d6b646ebad9755bcb805ab2d55
andkristine <kristine.andrejeva33@gmail.com>
2021-09-30 09:43:38 +0300

    tests added

--- src/test/java/com/accenture/challenge/ChallengeApplicationTests.java
+++ src/test/java/com/accenture/challenge/ChallengeApplicationTests.java
@@ -1,22 +1,45 @@
 package com.accenture.challenge;
 
+import com.accenture.challenge.model.Product;
+import com.accenture.challenge.repository.ProductRepositoryImpl;
 import com.accenture.challenge.service.ProductServiceImpl;
 import org.junit.Before;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class ChallengeApplicationTests {
 
 	ProductServiceImpl psi;
+	List<Product> sampleCart;
+	ProductRepositoryImpl pri;
 
 	@Before
 	public void setUp() {
 		psi = new ProductServiceImpl();
+		pri = new ProductRepositoryImpl();
+		sampleCart = new ArrayList<Product>();
+		sampleCart.add(new Product(1, 36, Product.Color.GREY, Product.Gender.UNISEX));
 	}
 
 	@Test
-	public void testFindById() {
+	public void addNewProductToCart() {
+		sampleCart.add(new Product(10, 46, Product.Color.RED, Product.Gender.MALE));
+		System.out.println(sampleCart.toString());
+	}
 
+	@Test
+	public void addProductToCartWithExistingId() {
+		sampleCart.add(new Product(1, 46, Product.Color.RED, Product.Gender.MALE));
+		System.out.println(sampleCart.toString());
+	}
+
+	@Test
+	public void getProductByNonExistingId() {
+		Product product = pri.getProductById(2, sampleCart);
+		System.out.println(product.toString());
 	}
 
 }
